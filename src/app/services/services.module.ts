4a1ab6e95399b94c77d0d1cdded1ce8b0cfe4864
rogervieiraa <rogeramorinvieiraa@gmail.com>
2020-08-10 16:26:14 -0300

    add minimax

---
+++ src/app/services/services.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MinimaxService } from './minimax.service';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  providers: [
+    MinimaxService
+  ]
+})
+export class ServicesModule { }
