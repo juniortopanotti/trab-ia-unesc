1673f6911f91651c87569c1ce6b9c4af9ca718af
Roger Vieira <rogeramorinvieiraa@gmail.com>
2020-08-11 17:39:36 -0300

    start updates

--- src/providers/min-max/min-max.ts
+++ src/providers/min-max/min-max.ts
@@ -12,7 +12,116 @@ export class MinMaxProvider {
     1: 1,
     2: -1,
   };
+  verificaGanhador(board, currentCol, currentRow) {
 
+
+        // Para verificar se alguma condição de vitória ou perda é alcançada. Retorna 1 se ganhar e "else" retorna 0  se perder.
+        // board [] é a matriz do jogo 7x6
+        // currentCol é o número da coluna em que o último token foi colocado 
+        // currentRow é o número da linha em que o último token foi colocado 
+        // O numero maximo de colunas é 7
+        // O numero maximo de linahs é 6
+    
+    
+        let newToken = board[currentRow][currentCol]; //jogador
+        let count = 0;//contador se chegar a 4 = vitória
+    
+        // Horizontal 
+        for (let i = 0; i < 7; i++) {
+            if (board[currentRow][i] == newToken)
+                count++;
+            else
+                count = 0;
+    
+            if (count >= 4)
+                return newToken;
+        }
+    
+        //vertical
+        for (let i = 0; i < 6; i++) {
+            if (board[i][currentCol] == newToken)
+                count++;
+            else
+                count = 0;
+    
+            if (count >= 4)
+                return newToken;
+        }
+    
+        // 4 na diagonal 1
+        //linha 0 até linha 5
+        for (let guideLine = 0; guideLine < 6 - 3; guideLine++) {
+            count = 0;
+            let row, col;
+            for (row = guideLine, col = 0; row < 6 && col < 7; row++, col++) {
+                if (board[row][col] == newToken) {
+                    count++;
+                    if (count >= 4) {
+                        return newToken;
+                    }
+                }
+                else {
+                    count = 0;
+                }
+            }
+        }
+        // 4 na diagonal 
+        //coluna 0 até colina 6
+        for (guideColumn = 1; guideColumn < 7 - 3; guideColumn++) {
+            count = 0;
+            let row, col;
+            for (row = 0, col = guideColumn; row < 6 && col < 7; row++, col++) {
+                if (board[row][col] == newToken) {
+                    count++;
+                    if (count >= 4) {
+                        return newToken;
+                    }
+                }
+                else {
+                    count = 0;
+                }
+            }
+        }
+    
+        // 4 na diagonal inversa
+        //linha 0 até linha 5
+        for (let guideLine = 0; guideLine < 6 - 3; guideLine++) {
+            count = 0;
+            let row, col;
+            for (row = guideLine, col = 6; row < 0 && col < 6; row++, col--) {
+                if (board[row][col] == newToken) {
+                    count++;
+                    if (count >= 4) {
+                        return newToken;
+                    }
+                }
+                else {
+                    count = 0;
+                }
+            }
+        }
+    
+        // 4 na diagonal inversa
+        //coluna 0 até colina 6
+        for (let guideColumn = 5; guideColumn > 2; guideColumn--) {
+            count = 0;
+            let row, col;
+            for (row = 0, col = guideColumn; row < 6 && col > 2; row++, col--) {
+                if (board[row][col] == newToken) {
+                    count++;
+                    if (count >= 4) {
+                        return newToken;
+                    }
+                }
+                else {
+                    count = 0;
+                }
+            }
+        }
+    
+        return 0;
+    }
+  
   fazMovimento(board, j, player) {
     for (let i = 0; i < 6; i++) {
       if (board[i][j] != this.branco) {
@@ -35,7 +144,7 @@ export class MinMaxProvider {
     return posicoes;
   }
 
-  moveIA(board, player) {
+  moveIA(board, player, difficulty = 7) {
     let possibilities = this.getPosicoes(board);
     let best_value = null;
     let best_move = null;
@@ -43,7 +152,7 @@ export class MinMaxProvider {
     for (let possibility of possibilities) {
       // Talvez tenha que fazer json parse unparse
       let board_aux = this.fazMovimento(board, possibility, player);
-      let value = this.minimax(board_aux, player);
+      let value = this.minimax(board_aux, player, 1, difficulty);
 
       if (best_value == null) {
         best_value = value;
@@ -60,11 +169,14 @@ export class MinMaxProvider {
     return best_move;
   }
 
-  minimax(board, player) {
+  minimax(board, player, deaph, difficulty) {
     if (player == 1) {
       player = 2;
     } else {
       player = 1;
+    }
+    if(deaph == difficulty) {
+      return 0;
     }
 
     let possibilities = this.getPosicoes(board);
@@ -74,7 +186,7 @@ export class MinMaxProvider {
       let possibility = possibilities[i];
       // Talvez tenha que fazer json parse unparse
       let board_aux = this.fazMovimento(board, possibility, player);
-      let value = this.minimax(board_aux, player);
+      let value = this.minimax(board_aux, player, deaph + 1, difficulty);
 
       if (best_value == null) {
         best_value = value;
